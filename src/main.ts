f540418a8032854fd6b4fe2bc8e0883d7ef03bc5
Bennett Smrdel <bennett@midwestdiesel.com>
2024-03-27 10:37:26 -0500

    chore: Create ts types

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import './style.css';
 import { basicSetup, EditorView } from "codemirror";
 import { javascript } from "@codemirror/lang-javascript";
 
-const template = 'var num1 = 5;\nvar num2 = 10;\nvar print num1 + num2;';
+const template = 'var num1 = 5;\nvar num2 = 10;\nprint num1 + (num2 - 5) * 8;';
 
 const editor = new EditorView({
   doc: template,
---
+++ src/scripts/types.d.ts
@@ -0,0 +1,55 @@
+interface Program {
+  
+}
+
+interface BinaryOperation {
+  operator: string
+  left: number
+  right: number
+}
+
+interface Conditional {
+  left: number | Variable
+  right?: number | Variable
+  conjunction?: string
+}
+
+interface Statement {
+  type: 'If' | 'While'
+  conditional: Conditional
+  body: any[]
+}
+
+type Variable = {
+  name: string
+  value: string | number | null | Variable
+};
+
+type Token = {
+  type: 'Var' |
+  'Number' |
+  'String' |
+  'Identifier' |
+  'Equals' |
+  'Operator' |
+  'LessThan' |
+  'GreaterThan' |
+  'LessThanEqual' |
+  'GreaterThanEqual' |
+  'LParen' |
+  'RParen' |
+  'LBrace' |
+  'RBrace' |
+  'LBracket' |
+  'RBracket' |
+  'If' |
+  'While' |
+  'Conjunction' |
+  'Print' |
+  'Semicolon' |
+  'PlusEquals' |
+  'MinusEquals' |
+  'TimesEquals' |
+  'DivideEquals'
+  value: string
+};
