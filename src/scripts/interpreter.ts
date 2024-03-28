6c102125f1f851d7b060e7457177d479abe920b1
Bennett Smrdel <bennett@midwestdiesel.com>
2024-03-28 15:45:26 -0500

    feat: Can interpret print statements

--- src/scripts/interpreter.ts
+++ src/scripts/interpreter.ts
@@ -8,28 +8,37 @@ export const interpreter = (program: Program) => {
   const { params, body } = program;
   body.forEach((stmt) => {
     if (stmt.type === 'Var') {
-      const { error } = declareVar(stmt);
+      const { error } = declareVar(stmt.value);
+      if (error) return error;
+    } else if (stmt.type === 'Print') {
+      const { error } = print(stmt.value);
       if (error) return error;
     }
   });
-  return { error: '' };
+  return { error: false };
 };
 
 const declareVar = (stmt: any) => {
   const value = parseValue(stmt.value);
-  if (stmt.value.value[0].type === 'Number' || stmt.value.value.length > 1) {
-    variables.set(stmt.value.name, Number(value));
+  if (stmt.value[0].type === 'Number' || stmt.value.length > 1) {
+    variables.set(stmt.name, Number(value));
   } else {
-    variables.set(stmt.value.name, value);
+    variables.set(stmt.name, value);
   }
-  console.log(getVar(stmt.value.name));
+  console.log(getVar(stmt.name));
+  return { error: false };
+};
+
+const print = (stmt: any) => {
+  const value = parseValue(stmt);
+  console.log('PRINT: ', value);
   return { error: false };
 };
 
 const parseValue = (token: any) => {
-  if (isMathExpression(token.value as any)) {
-    return parseMathExpression(token.value);
+  if (isMathExpression(token as any)) {
+    return parseMathExpression(token);
   } else {
-    return token.value ? token.value[0].value : null;
+    return token[0].value ? token[0].value : null;
   }
 };
